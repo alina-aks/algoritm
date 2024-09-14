d237d839f6121b5450ffbb21ba8652f74aa0a82f
alina-aks <aksyanova.alina@gmail.com>
2024-09-15 01:15:20 +0300

    tsk 1

--- lab0/task 1.3.py
+++ lab0/task 1.3.py
@@ -1,5 +1,9 @@
 f = open("input.txt")
 a,b = map(int, f.readline().split())
-summ = str(a+b)
-k = open("output.txt", "w")
-k.write(summ)
+if (-10**9<=a<=10**9) and (-10**9<=b<=10**9):
+    summ = str(a+b)
+    k = open("output.txt", "w")
+    k.write(summ)
+    print(a+b)
+else:
+    print("Условия для чисел не выполнены")
--- lab0/task1.2.py
+++ lab0/task1.2.py
@@ -1,2 +1,5 @@
 a, b = map(int,input().split())
-print(a+b*b)
+if (-10**9<=a<=10**9) and (-10**9<=b<=10**9):
+    print(a+b*b)
+else:
+    print("Условия для чисел не выполнены")
